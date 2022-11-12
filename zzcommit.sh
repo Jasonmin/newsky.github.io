a415f2ba7cb37a6bd88ba17ed44d4953d6064fa6
qiangzhou3 <qiangzhou3@iflytek.com>
2022-11-12 21:47:35 +0800

    22-11-12,21:47

--- zzcommit.sh
+++ zzcommit.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
 ## 权限授予 chmod u+x *.sh
+# chmod u+x ./zzcommit.sh
 
-# 更新css
-# cp outmd/style.css ./out
-# cp outmd/thirdmd/thirdstyle.css ./out
+# 获取提交信息
+message="`date +%y-%m-%d,%H:%M`"
+if [ "$1" != "" ] #前后保留空格
+then
+    message="$1"
+fi
 
-# cp assets ./out/assets
+# 日志
+# echo $message
 
 # 拉取
 git pull
 
-# 代码git提交
+# 提交并推送
 git add .
-git commit -m "`date +%Y-%m-%d,%H:%m`"
+git commit -m "$message"
 git push
